34b1c06b77538db2f1226983ed4087e7e69eb036
LeitAtar <121393718+LeitAtar@users.noreply.github.com>
2023-12-07 01:30:47 +0200

    start

---
+++ app/src/main/java/com/example/restaurantapp/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.restaurantapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraintsScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.restaurantapp.ui.theme.CardItemBg
+import com.example.restaurantapp.ui.theme.IconColor
+import com.example.restaurantapp.ui.theme.RestaurantAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RestaurantAppTheme {
+                // A surface container using the 'background' color from the theme
+
+            }
+        }
+    }
+}
+
+@Composable
+fun BoxWithRes(
+    resId: Int,
+    description: String,
+    bgColor: Color? = CardItemBg,
+    iconColor: Color? = IconColor,
+    boxSize: Int? = 40,
+    iconSize: Int = 24
+) {
+    Box(
+        modifier = Modifier
+            .size(boxSize!!.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(bgColor!!),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            painter = painterResource(id = resId),
+            contentDescription = description,
+            tint = iconColor!!,
+            modifier = Modifier.size(iconSize.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RestaurantAppTheme {
+        BoxWithRes(resId = R.drawable.menu, description = "menu")
+    }
+}
